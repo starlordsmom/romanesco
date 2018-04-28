5ba6409ce8a5a57ada9956b2b3ee68d4f465038c
starlordsmom <ashaitar@yahoo.com>
2018-04-28 11:08:39 +0200

    returned LR to default but changed NUM_STEPS to 20, let's see what that does

--- romanesco/const.py
+++ romanesco/const.py
@@ -7,6 +7,6 @@ MODEL_FILENAME = 'model'
 VOCAB_FILENAME = 'vocab.json'
 
 # Ugly hardcoded hyperparameters
-NUM_STEPS = 35 # truncated backprop length
-LEARNING_RATE = 0.00001
+NUM_STEPS = 20 # truncated backprop length
+LEARNING_RATE = 0.0001
 HIDDEN_SIZE = 1500 # layer size
